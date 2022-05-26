2250b210b3768c96702df16a1e612f4a52977a86
Pablo Egido <pgarciaegido@gmail.com>
2022-05-26 11:08:38 +0200

    Setting up repro

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { createUseStyles } from "react-jss";
+
 
 function App() {
+  const classes = createUseStyles({
+    paragraph: {
+      color: "red"
+    }
+  })();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <main>
+        <p className={classes.paragraph}>Red</p>
+      </main>
   );
 }
 
